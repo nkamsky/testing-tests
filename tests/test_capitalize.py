0871e149c0f7639e8e18ecf0e1cea047a18f14f2
Dmitry Krasnov <atlanticl@yandex.ru>
2024-03-05 21:50:48 +0300

    simple tests changed to asserts

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,8 +1,5 @@
 from package_name.capitalize import capitalize
 
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-print('Все тесты пройдены!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
